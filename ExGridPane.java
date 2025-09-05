e6af12762041a670dcfd80fbb7dc3ec1f4c923dc
Paulo Henrique da Rosa <101262901+paulodarosa7@users.noreply.github.com>
2025-09-04 22:41:38 -0300

    Update ExGridPane.java

--- ExGridPane.java
+++ ExGridPane.java
@@ -1,8 +1,12 @@
 package application;
 
+
+
 import javafx.application.Application;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -11,23 +15,32 @@ public class ExGridPane extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		try {
-			// criar botoes
-			Button btn1 = new Button("Botão 1");
-			Button btn2 = new Button("Botão 2"); // button
-			Button btn3 = new Button("Botão 3"); // left
-			Button btn4 = new Button("Botão 4"); //right
-			Button btn5 = new Button("Botão 5"); //center
-			Button btn6 = new Button("Botão 6");
+			TextField nome = new TextField();
+			TextField tel = new TextField();
+			TextField email = new TextField();
+			TextField cdd = new TextField();
+			
+			Label nomeL = new Label("Nome:");
+			Label telL = new Label("Telefone:");
+			Label emailL = new Label("Email:");
+			Label cddL = new Label("cidade:");
+
+
 			
 			// criar border pane e adicionar botoes
             GridPane gridPane = new GridPane();
 			
-			gridPane.add(btn1, 2,3);
-			gridPane.add(btn2,0,0);
-			gridPane.add(btn3,1,1);
-			gridPane.add(btn4,2,1);
-			gridPane.add(btn5,2,2);
-			gridPane.add(btn6,2,2);
+            gridPane.add(nomeL, 0, 0);
+            gridPane.add(nome, 1, 0);
+
+            gridPane.add(emailL, 0, 1);
+            gridPane.add(email, 1, 1);
+            
+            gridPane.add(telL, 0, 2);
+            gridPane.add(tel, 1, 2);
+
+            gridPane.add(cddL, 0, 3);
+            gridPane.add(cdd, 1, 3);
 
 
 
@@ -37,10 +50,11 @@ public class ExGridPane extends Application {
 			// criar espaçamento entre linhas e colunas
 			gridPane.setHgap(10);
 			gridPane.setVgap(10);
-			
+			gridPane.setPadding(new Insets(10, 20, 10, 20)); 
+
 			//criar stage
 			primaryStage.setScene(scene);
-			primaryStage.setTitle("Exemplo GridPane");
+			primaryStage.setTitle("GridPane");
 			primaryStage.show();
 
 
